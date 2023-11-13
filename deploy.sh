6aafa773e0f40cc8701802936e82230722dbaa4a
Prashant Srivastava <prashants@microsoft.com>
2023-11-13 11:57:07 +0530

    Styling updates, fixed props warning React

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 # Stop and remove existing Docker containers
+echo "Stopping and removing existing Docker containers..."
 sudo docker-compose down
 
 # Remove the existing Docker image
+echo "Removing existing Docker image..."
 sudo docker image rm stock-strategies_app:latest
 
 # Pull the latest changes from the Git repository
-git pull
+echo "Pulling the latest changes from the Git repository..."
+git pull orgin main
+
+# Build frontend assets
+echo "Building frontend assets..."
+cd frontend
+yarn install
+yarn build
+cd ..
 
 # Build and start Docker containers
+echo "Building and starting Docker containers..."
 sudo docker-compose up -d --scale app=1 --build
 
 # Display a message indicating successful execution
